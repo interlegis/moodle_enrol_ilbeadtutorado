09213b33091dd98a6234f64e2d49249d07229106
Marco Rougeth <marco@rougeth.com>
2014-11-25 15:45:09 -0200

    Validade da matrícula pela data considera apenas dia/mês/ano

--- lib.php
+++ lib.php
@@ -294,7 +294,9 @@ class enrol_ilbeadtutorado_plugin extends enrol_plugin {
                 $enrol = enrol_get_plugin('ilbeadtutorado');
                 $timestart = time();
                 if ($instance->enrolperiod) {
-                    $timeend = $timestart + $instance->enrolperiod;
+                    $timeend = date("Y/m/d", (string)($timestart + $instance->enrolperiod));
+                    list($year, $month, $day) = split("/", $timeend);
+                    $timeend = (int)strtotime("$year-$month-$day 23:59:59");
                 } else {
                     $timeend = 0;
                 }
